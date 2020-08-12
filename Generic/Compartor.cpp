e7f88e30d920a05a3d0c8a65dc21d1b83af03877
Ishaan Jain <ishaan160799@gmail.com>
2020-08-12 21:37:56 +0530

    Comparator Class:Book class created

--- Generic/Compartor.cpp
+++ Generic/Compartor.cpp
@@ -3,9 +3,35 @@
 //
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
+class Book {
+public:
+    string name;
+    int price;
 
+    Book() {
+
+    }
+
+    Book(string name, int price) {
+        this->price = price;
+        this->name = name;
+    }
+};
+
+int main() {
+    Book b1("C++", 100);
+    Book b2("Python", 120);
+    Book b3("Java", 130);
+    Book b4(b1);
+
+    list<Book> list;
+    list.push_back(b1);
+    list.push_back(b2);
+    list.push_back(b3);
+
+    Book bookToFind("C++", 110); //new edition
     return 0;
 }
