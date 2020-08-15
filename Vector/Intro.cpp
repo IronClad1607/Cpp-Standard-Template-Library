2965dadafb3bac3125bbcd5b9541158fce41cdef
Ishaan Jain <ishaan160799@gmail.com>
2020-08-15 16:34:37 +0530

    vector:Intro + initialise

---
+++ Vector/Intro.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ishaan Jain on 15-08-2020.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    //Ways to initiate a vector
+    vector<int> a;
+    vector<int> b(5, 10);
+    vector<int> c(b.begin(), b.end());
+    vector<int> d{1, 2, 4, 5, 13, 12};
+    return 0;
+}
