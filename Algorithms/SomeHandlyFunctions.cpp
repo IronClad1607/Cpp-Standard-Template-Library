596fc37bdf5825eec9cbcc2d3ddc8d47e17e3916
Ishaan Jain <ishaan160799@gmail.com>
2020-08-15 13:47:41 +0530

    handy:swap done

---
+++ Algorithms/SomeHandlyFunctions.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ishaan Jain on 15-08-2020.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    swap(a, b);
+
+    cout << a << " " << b << endl;
+}
+
