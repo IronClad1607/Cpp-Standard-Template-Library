36624a41a871ac2fbb23346210a467765d29e5b9
Ishaan Jain <ishaan160799@gmail.com>
2020-08-15 16:43:29 +0530

    vector:transvering donr

--- Vector/Intro.cpp
+++ Vector/Intro.cpp
@@ -12,5 +12,25 @@ int main() {
     vector<int> b(5, 10);
     vector<int> c(b.begin(), b.end());
     vector<int> d{1, 2, 4, 5, 13, 12};
+
+    //Ways to Iterate
+    //Classic For loop
+    for (int i = 0; i < c.size(); ++i) {
+        cout << c[i] << " ";
+    }
+    cout<<endl;
+
+    //With Pointers
+    for (auto it = c.begin(); it != c.end(); it++) {
+        cout << (*it) << " ";
+    }
+    cout<<endl;
+
+    //For each loop
+    for (int x:d) {
+        cout << x << " ";
+    }
+    cout<<endl;
+
     return 0;
 }
