5b218a9d7f8f29be5fb4182ec817d3344aaf4b98
Ishaan Jain <ishaan160799@gmail.com>
2020-08-15 13:42:43 +0530

    nextpermutation: rotate done

---
+++ Algorithms/NextPermutation.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Ishaan Jain on 15-08-2020.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    //rotate
+    rotate(v.begin(), v.begin() + 1, v.end());
+    for (int i = 0; i < v.size(); ++i) {
+        cout << v[i] << " ";
+    }
+
+//    //nextPermutation
+//    next_permutation(v.begin(), v.end());
+//
+//    for (int x:v) {
+//        cout << x << " ";
+//    }
+    return 0;
+}
+
