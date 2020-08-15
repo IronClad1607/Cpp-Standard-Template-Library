5f7eb2669badee8a50f32e53df55f5b5eeec6539
Ishaan Jain <ishaan160799@gmail.com>
2020-08-15 13:52:38 +0530

    handy: array reverse

--- Algorithms/SomeHandlyFunctions.cpp
+++ Algorithms/SomeHandlyFunctions.cpp
@@ -4,6 +4,7 @@
 
 #include <bits/stdc++.h>
 
+typedef int i;
 using namespace std;
 
 int main() {
@@ -15,5 +16,12 @@ int main() {
 
     cout << "Max:" << max(a, b) << endl;
     cout << "Min:" << min(a, b) << endl;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+    reverse(arr, arr + n);
+    for (int x:arr) {
+        cout << x << " ";
+    }
 }
 
